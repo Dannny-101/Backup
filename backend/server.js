7549740af37f896b0fe7e818d7cfa36ebf1b6291
maneater7 <marwamozammal@gmail.com>
2026-05-22 18:52:01 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -139,5 +139,5 @@ cron.schedule('0 9 * * 1', async () => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
